0f93b0b33a81431d960d0083466b7af75185152c
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-25 20:34:13 -0700

    created movie and filter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    // API key here
+    const data = await fetch();
+    const movies = await data.json();
+    // console.log(movies);
+    // saving all the results in popular
+    setPopular(movies.results);
+  };
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
     </div>
   );
 }
