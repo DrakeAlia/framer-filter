57456e8e661dccc345d28604768b0549ef17da3e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-25 21:13:31 -0700

    add useState for filter, activeGenre, and a useEffect for when active genre changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import "./App.css";
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import Movie from "./Movie";
+import Filter from "./Filter";
 
 function App() {
   const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  // setting to 0 because 0 will be all ids for the genres
+  const [activeGenre, setActiveGenre] = useState(0);
 
   useEffect(() => {
     fetchPopular();
@@ -12,17 +16,24 @@ function App() {
 
   const fetchPopular = async () => {
     // API key here
-    const data = await fetch();
+    const data = await fetch("");
     const movies = await data.json();
     // console.log(movies);
     // saving all the results in popular
     setPopular(movies.results);
+    setFiltered(movies.results);
   };
 
   return (
     <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
       <div className="popular-movies">
-        {popular.map((movie) => {
+        {filtered.map((movie) => {
           return <Movie key={movie.id} movie={movie} />;
         })}
       </div>
--- src/Filter.js
+++ src/Filter.js
@@ -1,13 +1,43 @@
 import { useEffect } from "react";
 
-function Filter() {
-    return (
-        <div className="filter-container">
-            <button>All</button>
-            <button>Comedy</button>
-            <button>Action</button>
-        </div>
-    )
+function Filter({ setActiveGenre, activeGenre, setFiltered, popular }) {
+  // run this function everytime our active genre(the buttons) changes
+  useEffect(() => {
+    if (activeGenre === 0) {
+      setFiltered(popular);
+      return;
+    }
+    const filtered = popular.filter((movie) =>
+      movie.genre_ids.includes(activeGenre)
+    );
+    // setFiltered to the filtered variable
+    setFiltered(filtered);
+  }, [activeGenre]);
+
+  // 35 and 28 are the genres ids for comedy and action based on the api docs for TMDB
+  return (
+    <div className="filter-container">
+      <button
+        // if activeGenre is 0 then add the active class to it, else don't
+        className={activeGenre === 0 ? "active" : ""}
+        onClick={() => setActiveGenre(0)}
+      >
+        All
+      </button>
+      <button
+        className={activeGenre === 35 ? "active" : ""}
+        onClick={() => setActiveGenre(35)}
+      >
+        Comedy
+      </button>
+      <button
+        className={activeGenre === 28 ? "active" : ""}
+        onClick={() => setActiveGenre(28)}
+      >
+        Action
+      </button>
+    </div>
+  );
 }
 
-export default Filter
+export default Filter;
